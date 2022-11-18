df88eb5382d9a584b97691cd501b6b7523a54391
schneiderkelsey <kelsey.schneider1988@gmail.com>
2022-11-18 07:00:57 -0500

    update to homework

--- data_1_homework/data_1_checks.py
+++ data_1_homework/data_1_checks.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # hello world statement
 print("hello world")
 
@@ -25,31 +24,3 @@ print(values[0])
 my_tuple = ("elderberry", "river birch", "common milkweed", "cardinal flower")
 
 print(my_tuple[2])
-=======
-# hello world statement
-print("hello world")
-
-# print list value
-my_list = [
-    "grasses/sedges/rushes", 
-    "small trees and shrubs", 
-    "ferns", 
-    "wildflowers"
-    ]
-
-print(my_list[2])
-
-# print dictionary value
-my_dict = {
-"small trees": "elderberry and river birch",
-"wildflowers": "common milkweed and cardinal flower"
-}
-values = list(my_dict.values()) 
-
-print(values[0])
-
-# print tuple value
-my_tuple = ("elderberry", "river birch", "common milkweed", "cardinal flower")
-
-print(my_tuple[2])
->>>>>>> 5a6d56690cdb8dc8797c6ea663ab86e8e4b86261
